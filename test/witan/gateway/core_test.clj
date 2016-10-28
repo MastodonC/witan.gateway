eb68857e8e351b2c8a84c0105cdb970158a12148
Antony Woods <antony@mastodonc.com>
2016-10-28 11:49:01 +0100

    Remove commented code

--- test/witan/gateway/core_test.clj
+++ test/witan/gateway/core_test.clj
@@ -2,13 +2,3 @@
   (:require [cheshire.core :as cheshire]
             [clojure.test :refer :all]
             [witan.gateway.handler :refer :all]))
-
-#_(defn parse-body [body]
-    (cheshire/parse-string (slurp body) true))
-
-#_(deftest a-test
-    (testing "Test GET request to /hello?name={a-name} returns expected response"
-      (let [response (app (-> (mock/request :get  "/api/plus?x=1&y=2")))
-            body     (parse-body (:body response))]
-        (is (= (:status response) 200))
-        (is (= (:result body) 3)))))
