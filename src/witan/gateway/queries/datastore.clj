615df8411f7af0be35708921acdf1f1599250118
Antony Woods <antony@mastodonc.com>
2017-01-20 16:48:46 +0000

    Further fixes to get the resolve

--- src/witan/gateway/queries/datastore.clj
+++ src/witan/gateway/queries/datastore.clj
@@ -31,39 +31,44 @@
   nil)
 
 (defn expand-metadata
-  [u d body]
-  (update-items
-   body
-   (fn expanding-item [item]
-     (let [prov-user-id
-           (get-in item [:kixi.datastore.metadatastore/provenance
-                         :kixi.user/id])
-           user-info (heimdall/get-users-info u d [prov-user-id])]
-       (if (:error user-info)
-         (error (format "Heimdall failed to return user information for %s: %s"
-                        prov-user-id
-                        user-info))
-         (let [collected-groups (->>
-                                 (:kixi.datastore.metadatastore/sharing item)
-                                 (vals)
-                                 (reduce concat)
-                                 (set))
-               group-resp (:items (heimdall/get-groups-info u d collected-groups))]
-           (if (:error group-resp)
-             (error (format "Heimdall failed to return group information for %s: %s"
-                            collected-groups
-                            group-resp))
-             (let [group-info (reduce (fn [a m] (assoc a (:kixi.group/id m) m)) {} group-resp)]
-               (-> item
-                   (assoc-in [:kixi.datastore.metadatastore/provenance
-                              :kixi/user] (first (:items user-info)))
-                   (update :kixi.datastore.metadatastore/provenance dissoc :kixi.user/id)
-                   (update :kixi.datastore.metadatastore/sharing
-                           (fn expand-sharing [sharing]
-                             (zipmap (keys sharing)
-                                     (map (partial mapv group-info) (vals sharing))))))))))))))
+  [u d item]
+  (let [prov-user-id
+        (get-in item [:kixi.datastore.metadatastore/provenance
+                      :kixi.user/id])
+        user-info (heimdall/get-users-info u d [prov-user-id])]
+    (if (:error user-info)
+      (error (format "Heimdall failed to return user information for %s: %s"
+                     prov-user-id
+                     user-info))
+      (let [collected-groups (->>
+                              (:kixi.datastore.metadatastore/sharing item)
+                              (vals)
+                              (reduce concat)
+                              (set))
+            group-resp (heimdall/get-groups-info u d collected-groups)]
+        (if (:error group-resp)
+          (error (format "Heimdall failed to return group information for %s: %s"
+                         collected-groups
+                         group-resp))
+          (let [group-info (reduce (fn [a m] (assoc a (:kixi.group/id m) m))
+                                   {}
+                                   (:items group-resp))]
+            (-> item
+                (assoc-in [:kixi.datastore.metadatastore/provenance
+                           :kixi/user] (first (:items user-info)))
+                (update :kixi.datastore.metadatastore/provenance dissoc :kixi.user/id)
+                (update :kixi.datastore.metadatastore/sharing
+                        (fn expand-sharing [sharing]
+                          (zipmap (keys sharing)
+                                  (map #(->> (mapv group-info %)
+                                             (keep identity)
+                                             (vec)) (vals sharing))))))))))))
 
-b;; kixi.datastore.metadatastore
+(defn expand-metadatas
+  [u d body]
+  (update-items body (partial expand-metadata u d)))
+
+;; kixi.datastore.metadatastore
 
 (defn metadata-by-id
   [{:keys [kixi.user/id kixi.user/groups] :as u} d meta-id]
@@ -95,5 +100,5 @@ b;; kixi.datastore.metadatastore
                                 :body
                                 #(when %
                                    (json/parse-string % keyword))))]
-        (expand-metadata u d body))
+        (expand-metadatas u d body))
       {:error (str "invalid status: " (:status resp))})))
