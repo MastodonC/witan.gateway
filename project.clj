247973d16a5e513de0d08e963c6d2f28a51e1e7e
Antony Woods <antony@mastodonc.com>
2017-02-21 10:16:19 +0000

    New logging for our ELK setup

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
                  [aero "1.0.0-beta5"]
                  [clj-time "0.12.0"]
                  [zookeeper-clj "0.9.4"]
-                 [kixi/kixi.comms "0.1.22"]
+                 [kixi/kixi.comms "0.1.26"]
                  [buddy/buddy-sign "1.3.0"]]
   :source-paths ["src"]
   :profiles {:uberjar {:aot  :all
