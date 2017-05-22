21f6a42c0f4864e23f1f660fcfa175fdf71c85d2
Antony <acron1@gmail.com>
2017-05-22 11:22:25 +0100

    Add transit write handler for java.util.regex.Pattern (#84)

--- src/witan/gateway/handler.clj
+++ src/witan/gateway/handler.clj
@@ -21,6 +21,9 @@
     (-> ips (clojure.string/split #",") first)
     (:remote-addr req)))
 
+(def regex-write-handler
+  (tr/write-handler "regex" (fn [o] (str o))))
+
 (def transit-encoding-level :json-verbose) ;; DO NOT CHANGE
 (defn transit-decode-bytes [in]
   (let [reader (tr/reader in transit-encoding-level)]
@@ -32,7 +35,8 @@
     (tr/read reader)))
 (defn transit-encode [s]
   (let [out (ByteArrayOutputStream. 4096)
-        writer (tr/writer out transit-encoding-level)]
+        writer (tr/writer out transit-encoding-level
+                          {:handlers {java.util.regex.Pattern regex-write-handler}})]
     (tr/write writer s)
     (.toString out)))
 
